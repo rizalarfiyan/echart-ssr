633dfd8c93168a5a38531c36cb9b8d5e566ac697
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2022-06-03 08:00:32 +0700

    feat: add base routing

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hello Word!')
+const http = require('http');
+
+const PORT = 8080;
+const server = http.createServer((req, res) => {
+    req.on('error', err => {
+        console.error(err);
+        res.statusCode = 400;
+        res.end('400: Bad Request');
+        return;
+    });
+
+    res.on('error', err => {
+        console.error(err);
+        res.statusCode = 500;
+        res.end('500: Internal Server Error');
+        return;
+    });
+
+    if (req.url === '/' && req.method === 'GET') {
+        res.end('Welcome!');
+        return;
+    }
+
+    res.statusCode = 404;
+    res.end('404: Page Not Found');
+});
+
+server.listen(PORT, () => {
+    console.log('Example app listening on port ' + PORT);
+});
